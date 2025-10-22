0db211554e1f8ee19343679d1df65ec36630f78f
maxx1559 <maxhbestle@hotmail.com>
2025-10-22 12:22:58 +0200

    Added new bindings to Program.cs and tested

--- Hospital.WebApi/Program.cs
+++ Hospital.WebApi/Program.cs
@@ -1,4 +1,6 @@
 using Hospital.Application;
+using Hospital.Application.Rules;
+using Hospital.Application.Rules.RuleObjects;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppointmentDbContext>(options =>
     options.UseInMemoryDatabase("HospitalDb"));
 builder.Services.AddScoped<AppointmentRepository>();
-builder.Services.AddScoped<AppointmentService>(); 
+builder.Services.AddScoped<AppointmentService>();
+builder.Services.AddScoped<NationalRegistryService>();
+
+
+
+//Add rule implementations
+builder.Services.AddScoped<IRule,ReferralRule>();
+builder.Services.AddScoped<IRule,InsuranceApprovalRule>();
+builder.Services.AddScoped<IRule,AssignedGPRule>();
+builder.Services.AddScoped<IRule,FinancialApprovalRule>();
+builder.Services.AddScoped<IRule,CprValidationRule>();
+builder.Services.AddScoped<IRule, UnknownDepartmentRule>();
+
+builder.Services.AddSingleton<IRuleDictionary>(sp =>
+    new RuleDictionary(
+        new Dictionary<string, List<IRule>>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["General Practice"] = new() { new AssignedGPRule() },
+            ["Surgery"] = new() { new ReferralRule() },
+            ["Radiology"] = new() { new ReferralRule(), new FinancialApprovalRule() },
+            ["Physiotherapy"] = new() { new ReferralRule(), new InsuranceApprovalRule() },
+            ["Cardiology"] = new() { new ReferralRule(), new FinancialApprovalRule(), new InsuranceApprovalRule() }
+
+        }));
 
 var app = builder.Build();
 
@@ -17,7 +42,7 @@ app.UseHttpsRedirection();
 
 app.MapPost("/appointments", async (AppointmentRequest request, AppointmentService appointmentService) =>
 {
-    var result = await appointmentService.ScheduleAppointment(
+    var result = await appointmentService.ScheduleAppointmentWithRules(
         request.Cpr, request.PatientName, request.AppointmentDate,
         request.Department, request.DoctorName);
 
