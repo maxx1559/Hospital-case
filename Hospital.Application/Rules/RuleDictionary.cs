6fb495716c6155b6717d75ae4fd13ce9cc7549ef
maxx1559 <maxhbestle@hotmail.com>
2025-10-22 12:15:23 +0200

    Added default rule for unknown departments

--- Hospital.Application/Rules/RuleDictionary.cs
+++ Hospital.Application/Rules/RuleDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hospital.Application.Rules.RuleObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Hospital.Application.Rules
 
         public IEnumerable<IRule> GetRulesForDepartment(string department)
         {
-            IEnumerable<IRule> rules = rulesForDepartment.TryGetValue(department, out var foundRules) ? foundRules : Enumerable.Empty<IRule>();
+            IEnumerable<IRule> rules = rulesForDepartment.TryGetValue(department, out var foundRules) ? foundRules : new List<IRule>() { new UnknownDepartmentRule() };
 
 
             return rules;
